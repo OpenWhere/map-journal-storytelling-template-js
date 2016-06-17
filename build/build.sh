01fe225ba635b1967226a535e3aa2e91e89fc162
owms <msanders@openwhere.com>
2016-06-17 10:40:13 -0600

    Add build files for CI/CD

---
+++ build/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Jenkins script to build a docker image for this project and upload to the docker registry
+#
+
+set -e
+
+if [ -z "$1" ]; then
+    TAG="latest"
+else
+    TAG=$1
+fi
+
+REGISTRY=${DOCKER_REGISTRY:-openwhere}
+
+grunt build
+npm prune --production
+
+docker build -t storymap-ui .
+docker tag -f storymap-ui $REGISTRY/storymap-ui:${TAG}
+docker push $REGISTRY/storymap-ui:${TAG}
