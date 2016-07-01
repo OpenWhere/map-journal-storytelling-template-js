ea489fcf112553a785c9735cefd85f8cb22a6030
Brian Poteat <bpoteat@openwhere.com>
2016-07-01 09:34:22 -0400

    Add an intermediate 'signin' page that allows reloading the app instead of routing directly to the AlertWhere signin page.

--- src/app/storymaps/common/blacksky/alertwhere-data.js
+++ src/app/storymaps/common/blacksky/alertwhere-data.js
@@ -1,31 +1,25 @@
-// This is currently hard-coded to return an array of some faked data. getData should be completed to
-// get the data from an exposed web service.
+// getData will query the StoryMaps API for the story data of the specific storymap ID. In the event of an
+// 'unauthorized' status code return, it will redirect to allow the user to login before reloading the storymap.
 
 define([], function () {
 
   function getData(id) {
-    console.log('retrieve data for id ' + id);
-    var new_data;
-    if (app.isProduction) {
-      $.ajax({
-        dataType: "json",
-        url: '/api/v1/storymaps/' + id,
-        async: false,
-        success: function (foo) {
-          new_data = foo;
+    console.log(`retrieve data for id ${id}`);
+    var newData = undefined;
+    $.ajax({
+      dataType: 'json',
+      url: `/api/v1/storymaps/${id}`,
+      async: false,
+      success: (response) => {
+        newData = response;
+      },
+      statusCode: {
+        401: () => {
+          window.location.href = `/storymaps/BlackSkySignin.html?appid=${id}`;
         }
-      });
-    } else {
-      $.ajax({
-        dataType: "json",
-        url: 'http://localhost:3000/api/v1/storymaps/' + id,
-        async: false,
-        success: function (foo) {
-          new_data = foo;
-        }
-      });
-    }
-    return new_data.events;
+      }
+    });
+    return newData.events;
   }
 
   return {
